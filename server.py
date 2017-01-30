ce00095ba7bc4d9f61e0770e7ab355694f365a36
Antoine Weber <antoine.weber@insa-lyon.fr>
2017-01-30 13:53:40 +0100

    ajout commentaires + mini modif

--- server.py
+++ server.py
@@ -9,6 +9,15 @@ import os
 
 version = "0.0.6"
 
+# Class Head = one player
+# Parameters :
+# name : pseudonyme
+# x,y,dxp,dxm,dyp,dym :position and current direction 
+# Alive : has he encontered a wall or no 1-> Alive / 0-> Dead
+# Id : ID given by the server 
+# Color : Color in #00000 format
+# wins : wins of the player since the server was started
+# totalwins : wins of the player when the server was started on this machine
 class Head:
 	def __init__(self,color,name):
 		self.name = name
@@ -23,10 +32,24 @@ class Head:
 		self.color = color
 		self.wins = 0
 		self.totalwins = 0
-		
+	
+	# Update position with the current speed(direction) of the Head
 	def move(self,wi,he,sp):
 		self.x = (self.x + self.dxp - self.dxm)%(wi)
 		self.y = (self.y + self.dyp - self.dym)%(he)
+
+# Class Game, omnipotent class that rules the game
+# sp : number of pixel per frame
+# list_Head : list of the head currently in the game
+# wall : table which tracks which cell on the grid is a wall or not
+# he : height of the game
+# wi : width of the game
+# sp : number of pixel of each cell
+# number_player : number of still alive player
+# ite : number of iteration of the game
+# StringIte : Keep each Position String for each iteration
+# timeNextIte: Time where the next iteration will occur
+# winner : Keep tracks of the winner of the last game
 
 class Game:
 	def __init__(self,wi,he,sp):
@@ -42,20 +65,22 @@ class Game:
 		self.timeNextIte = 0 
 		self.winner = 'None2'
 		
-		# Borders = wall ?
+		# On the native version our game is torical
+		# Decoment code bellow if you want borders to be walls 
 
-		# print self.wall.shape
 		# for i in range(wi):
 		# 	self.wall[i][0]=20
 		# 	self.wall[i][he-1]=20
 		# for i in range(he):
 		# 	self.wall[0][i]=20
 		# 	self.wall[wi-1][i]=20 
-
+	
+	
+	# Add a new player to the game
 	def addHead(self,h):
 		global AllTimePlayers
 		positionned = False
-		while not positionned:
+		while not positionned: # Try to find a spot not to close to other players
 			Ok = True
 			x = np.random.randint(self.wi)
 			y = np.random.randint(self.he)
@@ -63,13 +88,13 @@ class Game:
 				dist=0
 				dist+=abs(x-k.x)
 				dist+=abs(y-k.y)
-				if dist<25:
+				if dist<25: # Try to find a spot at least 25 blocks away from every other player
 					Ok = False
 			if Ok:
 				h.x=x
 				h.y=y
 				positionned=True
-		direction=np.random.randint(4)
+		direction=np.random.randint(4) # Set random starting direction to the player
 		if direction==0:
 			h.dxm=1
 		if direction==1:
@@ -82,47 +107,38 @@ class Game:
 		new = True
 		for p in AllTimePlayers:
 			if len(p)>0:
-				if p[0]==h.name:
+				if p[0]==h.name:  # Checks if the name is already know by the server
 					new = False
-					h.wins = p[2]
+					h.wins = p[2]		# Get the data for wins and totalwins
 					h.totalwins = p[1]
-		if new :
-			AllTimePlayers.append([h.name,0,0])
-					
-		h.Id=self.number_player
-		self.list_Head.append(h)
-		self.number_player+=1
+		if new : # If new player create a name in the server database (text based)
+			AllTimePlayers.append([h.name,0,0]) 
+											
+		h.Id=self.number_player # Server assigns an Id to the player
+		self.list_Head.append(h) # Player is added to the players currently in game
+		self.number_player+=1 
 	
-	def change(self,c,Id):
-		if c=='right':
-			swap = self.list_Head[Id].dym
-			self.list_Head[Id].dym = self.list_Head[Id].dxp
-			self.list_Head[Id].dxp = self.list_Head[Id].dyp
-			self.list_Head[Id].dyp = self.list_Head[Id].dxm
-			self.list_Head[Id].dxm = swap
-		if c=='left':
-			swap = self.list_Head[Id].dym
-			self.list_Head[Id].dym = self.list_Head[Id].dxm
-			self.list_Head[Id].dxm = self.list_Head[Id].dyp
-			self.list_Head[Id].dyp = self.list_Head[Id].dxp
-			self.list_Head[Id].dxp = swap
-	
+	# Method that make a real iteration of the game
 	def gameUpdate(self):
-		self.collision()
+		self.collision() # Check if a players die
 		for h in self.list_Head:      
-			self.wall[h.x][h.y]=h.Id  
-			h.move(self.wi,self.he,self.sp)
-			
+			self.wall[h.x][h.y]=h.Id   # Update wall table
+			h.move(self.wi,self.he,self.sp) # Move every player
+	
+	# Generate a string that holds the position of each player at the current iteration
 	def generateDataString(self):
 		s=str(self.ite)
-		if (self.ite%4)==0:
-			self.gameUpdate()
 		for h in self.list_Head:      
 			s+=" "+h.name+":"+str(h.x)+":"+str(h.y)+":"+str(h.color)
+		if (self.ite%4)==0: # Every 4 iteration make a real iteration
+			self.gameUpdate()
 		return s
 	
+	# If a client ask for an iteration strings returns it
 	def getString(self,n):
 		return self.StringIte[int(n)]
+	
+	# Check if a player collided with a wall
 	
 	def collision(self):
 		global endGame,endTime
@@ -132,9 +148,11 @@ class Game:
 				h.dxp=0
 				h.dym=0
 				h.dyp=0
+				# If yes : we loose a player and he isnt alive anymore
 				if h.alive==1:
 					self.number_player-=1
 					h.alive=0
+		# If only one player is alive, server pass in "endGame" phase
 		if self.number_player <2:
 			if not endGame:
 				print 'ending soon'
@@ -144,6 +162,7 @@ class Game:
 				for i in self.list_Head:
 					if i.alive==1:
 						self.winner = i.name
+	# In case of a new game resets all game variables
 	def reset(self):
 		global startingTime,speed,sp,wi,he
 		self.number_player = 0
@@ -153,15 +172,17 @@ class Game:
 		self.StringIte = []
 		self.timeNextIte = startingTime+ speed*sp
 		
+	# Give the names and wins of the players currenlty in the game
 	def returnName(self):
 		s='' 
 		for i in self.list_Head:
 			s+=i.name+':'+str(i.wins)+':'+str(i.totalwins)+' '
 		return s
 
+# Reset servers variables in case of a new game
 def newgame():
-	global delay,startingTime,endTime,endGame,nbParties
-	nbParties +=1
+	global delay,startingTime,endTime,endGame,nbGames
+	nbGames +=1
 	startingTime = time.time()+delay
 	updateHighscore(game.winner)
 	game.reset()
@@ -171,12 +192,14 @@ def newgame():
 	waitingForPlayers = True
 	
 	
+# Assign a random color to the player
 def getNewColor():
     r = random.randrange(0, 255)
     g = random.randrange(0, 255)
     b = random.randrange(0, 255)
     return '#%02x%02x%02x' % (r,g,b)
-    
+
+# Read the highcore textfile and load the informations in it
 def getHighscore():
 	global AllTimePlayers
 	if os.path.exists('highscore.txt'):
@@ -188,10 +211,11 @@ def getHighscore():
 				if len(t)>1:
 					AllTimePlayers.append([t[0],int(t[1]),0])
 		f.close()
-	else : 
+	else : # If he doesnt existe, create it
 		f = open('highscore.txt','w')
 		f.close()
 
+# Update the textfile with current information
 def updateHighscore(winner):
 	global AllTimePlayers
 	s = ''
@@ -204,39 +228,43 @@ def updateHighscore(winner):
 	f = open('highscore.txt','w')
 	f.writelines(s)
 	f.close()
-	    
+
+# Initialating Server global parameters
 wi = 60
 he = 48
 sp = 12
 speed = 0.0025
 delay = 3
 delayDeath = 2
-admin = True
 waitingForPlayers = True
 startingSoon = False 
 inGame = False
 endGame = False
-nbParties = 0
-startingTime =  time.time()+10000
+
+startingTime =  time.time()+delay
 endTime = time.time()+20000
 
+# Server connexions and game count
+nbGames = 0
 count = 0	
 
-
+# This method is runned each time a client connects to the server
 def handler(newsock):
 	currentTime = time.time() 
 	global count
 	count +=1
-	global waitingForPlayers,startingSoon,inGame,endGame,admin,startingTime,delay,sp,speed,endTime,endGame,nbParties,delayDeath
-	if inGame and currentTime>game.timeNextIte:
+	# Load global parameters
+	global waitingForPlayers,startingSoon,inGame,endGame,startingTime,delay,sp,speed,endTime,endGame,nbGames,delayDeath
+	if inGame and currentTime>game.timeNextIte: 
+		# If inGame and a iteration has passed do a new iteration
 		s=game.generateDataString()
 		game.StringIte.append(s)
 		game.timeNextIte+=speed*sp
 		game.ite+=1
-	if  waitingForPlayers and currentTime>(startingTime-1):
+	if  waitingForPlayers and currentTime>(startingTime-1): # Waiting -> Starting soon
 		waitingForPlayers = False
 		startingSoon = True
-	if startingSoon and currentTime>startingTime:
+	if startingSoon and currentTime>startingTime: # Starting soon -> inGame
 		startingSoon = False
 		inGame = True
 		s=game.generateDataString()
@@ -245,7 +273,7 @@ def handler(newsock):
 		game.ite+=1
 		endTime = time.time()+20000
 		print game.number_player
-		if game.number_player<2:
+		if game.number_player<2: # If only one player connected game doesn't start
 			waitingForPlayers = True
 			inGame = False
 			game.winner='None'
@@ -253,52 +281,53 @@ def handler(newsock):
 			print 'Game autoresetting'
 		#~ print game.StringIte
 	#~ print currentTime-(endTime+5)
-	if currentTime>(endTime+delayDeath):
+	if currentTime>(endTime+delayDeath): # if game has ended since delayDeath restart a new one
 		print 'You waited too long '
 		waitingForPlayers = True
 		inGame = False
 		game.winner=game.winner
 		newgame()
 		print 'Game autoresetting'
-		print count,nbParties
-	data = newsock.recv(size)
-	sd=data.split() 
+		print count,nbGames
+	data = newsock.recv(size) # Get the client message
+	sd=data.split()  # Split it
 	#~ print sd
-	if sd[0]=='name' and waitingForPlayers:
+	if sd[0]=='name' and waitingForPlayers: 
+		# If he tries to connect while we are waiting for players
+		# accept him and add his head to the game
 		color = getNewColor()
 		print "New player with pseudo %s joined the server"%sd[1]
 		h = Head(color,sd[1])
 		game.addHead(h)
-		if admin:
-			admin = False
-			startingTime = currentTime+delay
-			newsock.send('accepted '+str(wi) + ' ' + str(he) + ' ' + str(sp)+ ' ' +str(1)+ ' ' +str(h.Id)+ ' ' +str(startingTime)+ ' ' +str(h.dxm)+ ' ' +str(h.dxp)+ ' ' +str(h.dym)+ ' ' +str(h.dyp)+ ' ' +str(speed))
-		else:
-			newsock.send('accepted '+str(wi) + ' ' + str(he) + ' ' + str(sp)+ ' ' +str(0)+ ' ' +str(h.Id)+ ' ' +str(startingTime)+ ' ' +str(h.dxm)+ ' ' +str(h.dxp)+ ' ' +str(h.dym)+ ' ' +str(h.dyp)+ ' ' +str(speed))
-	if sd[0]=='?' and waitingForPlayers:
+		newsock.send('accepted '+str(wi) + ' ' + str(he) + ' ' + str(sp)+ ' ' +str(0)+ ' ' +str(h.Id)+ ' ' +str(startingTime)+ ' ' +str(h.dxm)+ ' ' +str(h.dxp)+ ' ' +str(h.dym)+ ' ' +str(h.dyp)+ ' ' +str(speed))
+	if sd[0]=='?' and waitingForPlayers: 
+		# if he thinks the game is still running while it isn't tell him 'NO'
 		newsock.send('NoGame '+str(startingTime)+' '+game.winner)
-	if sd[0]=='?' and inGame:
+	if sd[0]=='?' and inGame: # if the game is still running tell him the information is he asking for
 		msg = game.getString(sd[1])
-		#~ print nbParties
+		#~ print nbGames
 		newsock.send(msg)
-	if sd[0]=='move' and inGame:
+	if sd[0]=='move' and inGame: # If the game is running update his movement decision
 		h = game.list_Head[int(sd[2])]
 		h.dxm = int(sd[3])
 		h.dxp = int(sd[4])
 		h.dym = int(sd[5])
 		h.dyp = int(sd[6])
-	if sd[0]=='reset':
+	if sd[0]=='reset': # If he ask to reset the game, do so
 		waitingForPlayers = True
 		inGame = False
 		newgame()
 		print 'Game resetting'
-	if sd[0]=='who':
+	if sd[0]=='who': # If he ask who is connected do so
 		newsock.send(game.returnName())
 		#~ print game.returnName()
 	#~ print AllTimePlayers
-	newsock.shutdown(1)
+	newsock.shutdown(1) # Close socket
 	newsock.close()
 	
+	
+# Network stuff
+
 size = 1024
 listhead = []
 
@@ -307,21 +336,23 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('',3333))
 
 AllTimePlayers=[]
-getHighscore()
 
+getHighscore()
 
 game = Game(wi,he,sp)
 
 sock.listen(15)
 players = []
 
+# Loop that listen to the port 3333 and execute the "handler" method
+# each time a client connects to it
 while True:
 	newsock, addr = sock.accept()
 	players.append(newsock)
 	thr = threading.Thread(target = handler, args=(newsock,))
 	thr.start()
 	thr.join()
-	#~ if nbParties>10:
+	#~ if nbGames>10:
 		#~ break
 sock.shutdown(1)
 sock.close()
--- tron.py
+++ tron.py
@@ -11,6 +11,7 @@ version = "0.0.6"
 # Network parameters
 
 size = 1024
+port = 3333
 
 #~ host = '127.0.0.1'
 #~ host = 'bs406-s31-20.insa-lyon.fr'
@@ -19,6 +20,8 @@ size = 1024
 #~ player = 'insert_name_here'
 
 canvas= ''
+
+# Method used to make a User Interface in the terminal 
 
 def terminaldisplay():
 	global NotInGame,WaitingToStart,inGame,version,game,listPlayer,lastWinner,Initialisation
@@ -31,8 +34,10 @@ def terminaldisplay():
 	if Initialisation:
 		print ''
 	else :
-		print 'Last game winner : '+lastWinner 
+		print 'Last game winner : '+lastWinner # Print the winner of the last game
 		print ''
+		
+		# Print the status of the game 
 		if NotInGame:
 			print " Waiting for game "
 			for i in range(13):
@@ -40,7 +45,7 @@ def terminaldisplay():
 		if WaitingToStart:
 			global startingTime
 			numb=str(int(round(startingTime-time.time())))
-			print 'Game starting in '+numb+' seconds'
+			print 'Game starting in '+numb+' seconds' 
 			print ' '
 			print 'Players connected : '
 			print ' '
@@ -65,14 +70,22 @@ def terminaldisplay():
 		
 		
 
+# Method used to keep make the game display itself on the player's screen
+# Id Identifiant given by the server and used to communicate with him
+# wi,he,sp parameters of the graphic window given by the server
+# dxm, dxp, dym ,dyp current input of the player
+# ite : current iteration of the game
+# speed : parameter of the server
+# disp : variable to know when to update the terminal display
+# timeNextIte : Time of the next iteration
+# 
+
 class GameClient:
-    def __init__(self,wi,he,sp,ad,Id,startingTime,dxm,dxp,dym,dyp,speed):
+    def __init__(self,wi,he,sp,Id,startingTime,dxm,dxp,dym,dyp,speed):
 		self.Id = Id
-		self.ad = ad
 		self.wi = wi
 		self.he = he
 		self.sp = sp
-		self.number_player=0
 		self.dxm = dxm
 		self.dxp = dxp
 		self.dym = dym
@@ -81,15 +94,15 @@ class GameClient:
 		self.speed = speed
 		self.disp = 0
 		self.timeNextIte = startingTime+speed*self.sp
-		self.notLoadedPos = True
 		
-	
+	# Method that runs during the loading phase before a game
     def phase_init(self):
-		global NotInGame,WaitingToStart,inGame,startingTime,ad,listPlayer
+		global NotInGame,WaitingToStart,inGame,startingTime,listPlayer
 		wait = True
 		while wait:
 			currentTime = time.time()
 			if(currentTime < startingTime):
+				# Each second a pixel art number is displayed on screen
 				if round(startingTime-currentTime-round(startingTime-currentTime),5)==0:
 					numb = str(int(round(startingTime-currentTime)))
 					c=[0,0,0,0,0,0,0,0]
@@ -129,11 +142,13 @@ class GameClient:
 								col2='#C0E9E6'
 							canvas.create_rectangle((self.wi/2-2*(1-i))*self.sp,(self.he/2-2*(2-j))*self.sp,(self.wi/2+2*i)*self.sp,(self.he/2-2*(1-j))*self.sp,fill=col)
 							canvas.create_rectangle((self.wi/2-2*(4-i))*self.sp,(self.he/2-2*(2-j))*self.sp,(self.wi/2-4+2*i)*self.sp,(self.he/2-2*(1-j))*self.sp,fill=col2)
+					# Connect to the server and ask who is currently connected (waiting for the game)
 					sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 					sock.connect((host,port))
 					sock.send('who')
 					LP=sock.recv(size).split()
 					listPlayer=[]
+					# Store this information in the global variable listPlayer
 					for lp in LP:
 						p = lp.split(':')
 						listPlayer.append([p[0],p[1],p[2]])
@@ -159,6 +174,7 @@ class GameClient:
 				terminaldisplay()
 		
 	
+	# If the user press the Right or Left arrow key, direction is updated
     def change(self,c):
 		#~ print c
 		if c=='left':
@@ -174,33 +190,43 @@ class GameClient:
 			self.dyp = self.dxp
 			self.dxp = swap
 		
+	# Method that is looped by the window and makes the game run
     def draw(self):
-		currentTime=time.time()
+		currentTime=time.time() 
+		# Gets the current computer time
+		# Must be aligned on current network time else synchronisation 
+		# problems may occur
 		global NotInGame,WaitingToStart,inGame,startingTime,lastWinner
-		if inGame:
-			if currentTime > self.timeNextIte: 
+		if inGame: # If we are in a game
+			if currentTime > self.timeNextIte:  #If an iteration has passed 
+				# ask the server what happened last iteration
 				sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 				sock.connect((host,port))
 				ask = '? '+str(self.ite)
 				sock.send(ask)
 				tab = sock.recv(size).split()
 				
-				#~ if(np.random.random()<0.05):
-					#~ if(np.random.random()<0.5):
-						#~ self.change('right')
-					#~ else:
-						#~ self.change('left')
+				# Decoment code below if you want a player to have a random behaviour
 				
+				#if(np.random.random()<0.05):
+				#	if(np.random.random()<0.5):
+				#		self.change('right')
+				#	else:
+				#		self.change('left')
+				
+				# If the server reply that no game is currently running
+				# the procedure to reiniate the game is launched
 				if tab[0]=='NoGame':
 					sock.close()
 					inGame = False
 					NotInGame = True	
-					self.reset()
-					startingTime = float(tab[1])
-					lastWinner = tab[2]
+					self.reset() # Cleanse the canvas
+					startingTime = float(tab[1]) # New starting time told by the server
+					lastWinner = tab[2] # Get the winner of the last game
 					self.timeNextIte = startingTime+self.speed*self.sp
-					params=findGame()
+					params=findGame() # Try to reconnect to the new game
 					self.ite = 0
+					# Store the parameters of the new game
 					self.wi = int(params[1])
 					self.he = int(params[2])
 					self.sp = int(params[3])
@@ -210,9 +236,12 @@ class GameClient:
 					self.dym =  int(params[9])
 					self.dyp =  int(params[10])
 					self.speed = float(params[11])
+					# Launch new game procedure
 					game.begin()
 				else :
 					sock.close()
+					# If game is running update iteration variables and print 
+					# what occured in the last iteration
 					self.timeNextIte+=self.speed*self.sp
 					self.ite+=1
 					i=1
@@ -221,36 +250,36 @@ class GameClient:
 						canvas.create_rectangle(int(h[1])*self.sp,int(h[2])*self.sp,(int(h[1])+1)*self.sp,(int(h[2])+1)*self.sp,fill=h[3])
 						i+=1
 				
+			# Tell the server what direction the player is willing to go
 			sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			sock.connect((host,port))
 			ask = 'move '+str(self.ite)+' '+str(self.Id)+' '+str(self.dxm)+' '+str(self.dxp)+' '+str(self.dym)+' '+str(self.dyp)
 			sock.send(ask)
 			sock.close()
 			
+			# Close to every second update the terminal display
 			self.disp+=1
 			if self.disp>100:
 				terminaldisplay()
 				self.disp=0	
 				
     def begin(self):
-		global NotInGame,WaitingToStart,inGame,LabelNotCreated
-		if LabelNotCreated:
-			self.createLabel()
+		global NotInGame,WaitingToStart,inGame
+		# Launch the ' waiting for the game to start' phase
 		self.phase_init()
+		# Every 10 ms, execute the self.draw() method and update canvas
 		for i in range(100000):
 			window.after(10,self.draw())
 			window.update()			
 				
-			
-    def createLabel(self):
-		l=Label( window, font=('times', 20, 'bold'), bg='green',text='Host')
-		l.pack()
+	# Cleanse the canvas (liberate memory) and update canvas
     def reset(self):
 		global canvas
 		canvas.delete("all")
 		#~ canvas = Canvas(window,bg='black',height=he*sp,width=wi*sp)
 		window.update()
 
+# If the user press the 'newgame' button, ask server to launch a new game
 def newgame():
 	def start():
 		sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -259,44 +288,44 @@ def newgame():
 		print "Asking serveur to reset the game"
 		sock.send('reset')
 		sock.close()
-		
 	return start
 
+# Method used to periodicly ask the server if the player can join the game
 def findGame():
 	NotConnected = True
 	global NotInGame,WaitingToStart,inGame
 	while NotConnected:
-		
 		sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		sock.connect((host,port))
 		data = 'name '+player
 		sock.send(data)
 		s=sock.recv(size)
+		# while nothing is recivied wait
 		while sock.recv(size)!='':
 			s=sock.recv(size)
 		sock.close()
+		# if the server tells us 'yes' the pass in the next phase
 		if s.split()!=[]:
 			if s.split()[0]=='accepted':
 				NotInGame = False
 				WaitingToStart = True
 				params = s.split()
 				NotConnected = False
-		
 		terminaldisplay()
+		# Wait half a second between each attempt
 		if NotConnected:
 			time.sleep(0.5)
 	return params
 
-
-
+# Global variables used to know in which phase the client currenlty is
 NotInGame = True
 WaitingToStart = False 
 inGame = False
-LabelNotCreated = False
 
 Initialisation = True
 terminaldisplay()
-#~ 
+
+# Ask the user where is the server launched and what will be is username
 while True:
 	try:
 		#~ print ('WARNING : Please execute this script from a real terminal ')
@@ -304,8 +333,8 @@ while True:
 		print ('Where is the server launched ?')
 		print ('For example if it is on bs406-s31-15 enter 15')
 		print ''
-		x = int(input(""))
-		host = 'bs406-s31-'+str(x)
+		x = raw_input()
+		host = 'bs406-s31-'+x
 		break
 	except ValueError:
 		print("Oops!  That was no valid number.  Try again...")
@@ -314,7 +343,7 @@ terminaldisplay()
 
 while True:
 	try:
-		print('What is your name ?')
+		print('What is your username ?')
 		print('Example : xXDarkRangerdu93Xx or John')
 		print ''
 		s = raw_input("")
@@ -322,23 +351,21 @@ while True:
 		Initialisation = False
 		break
 	except ValueError:
-		print("Oops!  That was not a valid name.  Try again...")
+		print("Oops!  That was not a valid username.  Try again...")
 
-
-
-startingTime = 0
-port = 3333
+# Initialisation of global variables
 listPlayer=[]
 game=''
+startingTime = 0
 lastWinner = 'None'
 
-
+# Try to find a game
 params=findGame()
 
+# Store game parameters into global variables
 wi = int(params[1])
 he = int(params[2])
 sp = int(params[3])
-ad = int(params[4])
 Id = int(params[5])
 dxm =  int(params[7])
 dxp =  int(params[8])
@@ -347,22 +374,24 @@ dyp =  int(params[10])
 speed = float(params[11])
 
 startingTime = float(params[6])
+# Get current time
 currentTime = time.time()
 
-game = GameClient(wi,he,sp,ad,Id,startingTime,dxm,dxp,dym,dyp,speed)
+game = GameClient(wi,he,sp,Id,startingTime,dxm,dxp,dym,dyp,speed)
 
+# Launch a new window 
 window = Tk()
 window.title('TRON')
+# Create a new canvas of the size dictated by the server
 canvas = Canvas(window,bg='black',height=he*sp,width=wi*sp)
 canvas.pack(side=TOP)
 
-#~ if ad:
-
+# Create a "New Game" button
 b=Button(window,text="New Game")
 b.config(command=newgame())
 b.pack()
 
-
+# Add listener to Left and Right Arrow keys to allow the player to input directions
 window.bind('<Right>', lambda event: game.change('right'))
 window.bind('<Left>', lambda event: game.change('left'))
 window.update()
